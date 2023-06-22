affd403c179504e2c076e3b83ee6139436b5e905
Glenn O. Larsen <glenn.larsen@gmail.com>
2023-06-22 16:40:51 +0200

    Mass update :/

--- bin/shutdown.sh
+++ bin/shutdown.sh
@@ -7,5 +7,20 @@ if [ "$1" != "yes" ]; then
     exit
 fi
 
+# Turn off podman
+podman=$(ps aux | grep podman | grep -v grep | wc -l)
+if [ "${podman}" != "0" ]; then
+  echo -n "Stopping podman  "
+  sudo podman stop -a
+fi
+
+# Turn off docker
+systemctl status docker &> /dev/null
+if [ $? != 3 ]; then
+  echo -n "Stopping docker  "
+  systemctl stop docker
+fi
+
+
 shutdown -h +0
 
