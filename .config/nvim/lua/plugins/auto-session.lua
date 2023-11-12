738e6ff40adee3bd16e0e512da63f44e630c2181
Glenn O. Larsen <glenn.larsen@gmail.com>
2023-11-12 21:04:01 +0100

    Required config elements for plugin

--- .config/nvim/lua/plugins/auto-session.lua
+++ .config/nvim/lua/plugins/auto-session.lua
@@ -4,6 +4,12 @@ return {
     lazy = false,
     config = function()
       require("auto-session").setup({
+        auto_session_enabled = true,
+        auto_session_root_dir = vim.fn.stdpath("data") .. "/sessions/",
+
+        auto_session_enable_last_session = false,
+        auto_session_last_session_dir = vim.fn.stdpath("data") .. "/sessions/",
+
         log_level = "error",
         auto_session_suppress_dirs = { "~/", "~/Projects", "~/Downloads", "/" },
       })
